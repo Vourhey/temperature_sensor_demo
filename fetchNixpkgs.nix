d4572e8dec60a9a5376306a37a83ebe2cbe7f24f
Vadim Manaenko <vadim.razorq@gmail.com>
2019-12-06 17:39:48 +0300

    Update package dependencies

--- fetchNixpkgs.nix
+++ fetchNixpkgs.nix
@@ -1,5 +1,5 @@
-{ rev    ? "b365b36bbca7a542a29b6862c0a2d8db9846764f"             # The Git revision of nixpkgs to fetch
-, sha256 ? "0yfff0qxp2cfbncrfcbcifli4bsj2pzv482kn08m50p1ypp1ivik" # The SHA256 of the downloaded data
+{ rev    ? "30e950867bb08e3d1ee88bfa911eef0131262e30"             # The Git revision of nixpkgs to fetch
+, sha256 ? "0mj34j22ziafr17c6js97qqy6vh7vqxf9917dwjcq6zi8qm5qzcx" # The SHA256 of the downloaded data
 }:
 
 builtins.fetchTarball {
